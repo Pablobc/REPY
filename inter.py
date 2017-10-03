68f00c3e06909479cc429a9d9b45178baa5d8f92
Pablobc <31974681+Pablobc@users.noreply.github.com>
2017-10-03 17:19:04 -0300

    Update inter.py

--- inter.py
+++ inter.py
@@ -10,20 +10,22 @@ import re
 
 #expresiones regulares
 
-declaracion = re.compile('\s*let mut ([A-z]+_*[A-z]*) : (i16|i32|f64) = ((-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\))|(([A-z]+_*[A-z]*)\((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))\)));')
-asignacion = re.compile('\s*([A-z]+_*[A-z]*) = ((-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\))|(([A-z]+_*[A-z]*)\((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))\)));')
-operacion = re.compile('\s*((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\))\s*(\+|\-)\s*(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\)));?')
-casteo = re.compile('\s*(\(([A-z]+_*[A-z]*) as (i16|i32|f64)\));?')
+declaracion = re.compile('\s*let mut ([A-z]+_*[A-z]*) : (i16|i32|f64) = ((-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \))|(([A-z]+_*[A-z]*)\((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))\)));')
+asignacion = re.compile('\s*([A-z]+_*[A-z]*) = ((-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \))|(([A-z]+_*[A-z]*)\((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))\)));')
+operacion = re.compile('\s*((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \)));?')
+casteo = re.compile('\s*(\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \));?')
 ctrl_if = re.compile('\s*if ([A-z]+_*[A-z]*) (>|<|=|<=|>=) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)) {')
-ctrl_elseif = re.compile('\s*} else if ([A-z]+_*[A-z]*)\s*(>|<|=|<=|>=)\s*(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)) {')
+ctrl_elseif = re.compile('\s*} else if ([A-z]+_*[A-z]*) (>|<|=|<=|>=) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)) {')
 ctrl_else = re.compile('\s*} else {')
 fin = re.compile('\s*}')
-ctrl_while = re.compile('\s*while ([A-z]+_*[A-z]*)\s*(>|<|=|<=|>=)\s*(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)) {')
-boolean = re.compile('(\s*[A-z]+_*[A-z]*)\s*(>|<|=|<=|>=)\s*(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))')
-printeo = re.compile('\s*println!\s*\(([A-z]+_*[A-z]*)\);') 
+ctrl_while = re.compile('\s*while ([A-z]+_*[A-z]*) (>|<|=|<=|>=) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)) {')
+boolean = re.compile('(\s*[A-z]+_*[A-z]*) (>|<|=|<=|>=) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))')
+printeo = re.compile('\s*println!\(([A-z]+_*[A-z]*)\);') 
 funcioninit = re.compile('fn ([A-z]+_*[A-z]*) \(([A-z]+_*[A-z]*):(i16|i32|f64)\) - > (i16|i32|f64) {')
-ret = re.compile('\s*return\s*(([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\(([A-z]+_*[A-z]*) as (i16|i32|f64)\))|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+));')
+ret = re.compile('\s*return (([A-z]+_*[A-z]*)|(([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \)) (\+|\-) (([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+)|\( ([A-z]+_*[A-z]*) as (i16|i32|f64) \))|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+));')
 fun_main =re.compile('fn main\(\) {')
+funcion=re.compile("(([A-z]+_*[A-z]*)\((([A-z]+_*[A-z]*)|(-[0-9]+|[0-9]+|[0-9]+\.[0-9]+|-[0-9]+\.[0-9]+))\))")
+
 
 
 
@@ -35,35 +37,56 @@ fun_main =re.compile('fn main\(\) {')
 
 
 ###########################MAIN###################
+def guardar_fn(linea,dic):
+	varname=re.match(funcioninit,linea).group(2)
+	nombre=re.match(funcioninit,linea).group(1)
+	tipo_input=re.match(funcioninit,linea).group(3)
+	tipo_output=re.match(funcioninit,linea).group(4)
+	dic[nombre]=[]
+	dic[nombre].append(tipo_input)
+	dic[nombre].append(tipo_output)
+	dic[nombre].append([])
+	for linea in texto:
+		dic[nombre][2].append(linea.strip())
+		if re.match(ret,linea):
+			break
+	return 1
+def castear(linea,dic):
+	nombre=re.match(casteo,linea).group(1)
+	tipo=re.match(casteo,linea).group(2)
+	dic[nombre][1]=tipo
+	return
+def operar(linea):
+	operando1= re.match(operacion,linea).group(1)
+	operando2= re.match(operacion,linea).group(3)
+	operacion= re.match(operacion,linea).group(2)
+	if dic[operando1][1] != dic[operando2][1]:
+		print ''
+	if operacion == '+':
+
+
+	
+
+
+
 texto = open("codigorust.txt", 'r')
+main_var={}
 for linea in texto:
-	if re.match(declaracion,linea):
-		print ("declaracion detectada")
-	if re.match(asignacion,linea):
-		print("asignacion detectada")
-	if re.match(operacion,linea):
-		print("operacion detectada")
-	if re.match(casteo,linea):
-		print("casteo detectado")
-	if re.match(ctrl_if,linea):
-		print("if detectado")
-	if re.match(ctrl_elseif,linea):
-		print("elseif detectado")
-	if re.match(ctrl_else,linea):
-		print ("else detectado")
-	if re.match(ctrl_while,linea):
-		print("while detectado")
-	if re.match(fin,linea):
-		print ("fin de control o funcion detectado")
-	if re.match(boolean,linea):
-		print("booleano detectado")
-	if re.match(printeo,linea):
-		print("print detectado")
 	if re.match(funcioninit,linea):
-		print("inicio de funcion detectada")
-	if re.match(ret,linea):
-		print("return detectado")
-	if re.match(fun_main,linea):
+
+		guardar_fn(linea,main_var)
+print main_var
+	
+
+
+
+
+
+
+
+
+texto.close()
+
 		print("funcion main detectada")
 texto.close()
 
